b7732281ab7eadbb14531e3d6f971c806e35ac99
David James <davidj28827@gmail.com>
2017-06-11 09:38:30 +1000

    Moved stuff to functions and will start making web page :seedling:

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+//API Auth Token from Dropbox
+define("DROPBOX_TOKEN","rywD9hwWLDcAAAAAAAAxJwER93PT-olalIv0qmdDMHUZB2DZjqsz4tDWzfgonZCG");
+
+//Path relative to ROOT of Dropbox
+define("PHOTOS_FOLDER", "/Photos/FilmPhotos");
