f4b586d469154b8bd16ed9cec3d0375dbc96066b
David James <david.james@chromatix.com.au>
2017-06-22 16:32:18 +1000

    Deleted config

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//API Auth Token from Dropbox
-define("DROPBOX_TOKEN","rywD9hwWLDcAAAAAAAAxJwER93PT-olalIv0qmdDMHUZB2DZjqsz4tDWzfgonZCG");
-
-//Path relative to ROOT of Dropbox
-define("PHOTOS_FOLDER", "/Photos/FilmPhotos");
